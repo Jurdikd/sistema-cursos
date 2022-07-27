8f0cd39953c13161655dcfa1bb650cf643f1225a
Jurdikd <jesuscovo@gmail.com>
2022-07-27 19:45:53 -0400

    conexion

--- sistema/includes/config.php
+++ sistema/includes/config.php
@@ -2,13 +2,13 @@
 
 
 $host = 'localhost';
-$user = 'root';
-$db = 'sistema_cursos';
-$pass = '';
+$user = 'u839792224_sistema_cursos';
+$db = 'u839792224_sistema_cursos';
+$pass = 'Prom2204';
 
-    try {
-        $pdo = new PDO('mysql:host='.$host.';dbname='.$db.';charset=utf8',$user,$pass);
-        $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-    } catch (Exception $e) {
-        echo 'ERROR: '.$e->getMessage();
-    }
+try {
+    $pdo = new PDO('mysql:host=' . $host . ';dbname=' . $db . ';charset=utf8', $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (Exception $e) {
+    echo 'ERROR: ' . $e->getMessage();
+}
